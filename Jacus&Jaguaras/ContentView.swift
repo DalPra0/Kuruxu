2e3990ca123db61e8c476766150ac266e955c23f
DalPra0 <153088537+DalPra0@users.noreply.github.com>
2025-11-10 10:34:02 -0300

    Initial Commit

---
+++ Jacus&Jaguaras/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Jacus&Jaguaras
+//
+//  Created by Lucas Dal Pra Brascher on 10/11/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Jacus&Jaguaras/Jacus_JaguarasApp.swift
@@ -0,0 +1,17 @@
+//
+//  Jacus_JaguarasApp.swift
+//  Jacus&Jaguaras
+//
+//  Created by Lucas Dal Pra Brascher on 10/11/25.
+//
+
+import SwiftUI
+
+@main
+struct Jacus_JaguarasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
